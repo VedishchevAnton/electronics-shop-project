3a3ae2838205f2ca0fec301ad1c117e2d41feb47
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-09 22:21:46 +0300

    add test_item_apply_discount

--- tests/test_item.py
+++ tests/test_item.py
@@ -3,6 +3,7 @@ import pytest
 from src.item import Item
 
 
+#
 def test_item_init():
     """
     Тест инициации класса Item
@@ -12,4 +13,15 @@ def test_item_init():
     assert item1.name == 'Смартфон'
     assert item1.price == 10000
     assert item1.quantity == 20
+    assert isinstance(item1.name, str)
+    assert isinstance(item1.price, float)
+    assert isinstance(item1.quantity, int)
+
+
+def test_item_apply_discount():
+    """
+    Тест расчета общей стоимости конкретного товара
+    """
+    item1 = Item("Смартфон", 10000, 20)
+    assert int(item1.price * item1.quantity) == 200000
 
