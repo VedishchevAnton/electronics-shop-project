c4355ae992eb9ae76f48a80581465d51ffe653bb
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-09 22:09:25 +0300

    add test_item_init

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,3 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
-import item.py
+from src.item import Item
+
+
+def test_item_init():
+    """
+    Тест инициации класса Item
+    """
+    item1 = Item("Смартфон", 10000, 20)
+
+    assert item1.name == 'Смартфон'
+    assert item1.price == 10000
+    assert item1.quantity == 20
+
