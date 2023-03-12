c9fbbc3ec33de5d9be8d821db5ae382968aa1c13
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-12 21:09:57 +0300

    добавление функции получения данных из файла csv в файле utils.py

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+import csv
+
+
+def take_from_csv() -> list:
+    """
+    Получение данных из файла csv
+    """
+    data_list = []
+    with open('../src/items.csv', 'r') as csvfile:
+        data_csv = csv.reader(csvfile, delimiter=',')
+        for elem in data_csv:
+            if elem[0] == 'name':
+                continue
+            else:
+                data_list.append(elem)
+        return data_list
