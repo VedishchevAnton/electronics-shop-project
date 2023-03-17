94644803d1a57721d68004ca3b00ed7d1f078818
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-18 01:38:34 +0300

    change class Phone

--- src/phone.py
+++ src/phone.py
@@ -1,28 +1,30 @@
-from item import Item
+from src.item import Item
 
 
 class Phone(Item):
-    def __init__(self, name: str, price: float, sim_count: int, quantity: int):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         """
         Создание экземпляров класса Phone
-        @type sim_count: integer
+        @type number_of_sim: integer
         """
         super().__init__(name, price, quantity)
-        if sim_count > 0:
-            self.__sim_count = sim_count
-        else:
-            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        self.number_of_sim = number_of_sim
 
     @property
-    def sim_count(self):
-        return self.__sim_count
+    def number_of_sim(self):
+        return self.__number_of_sim
 
-    @sim_count.setter
-    def sim_count(self, sim_count: int):
-        if sim_count > 0:
-            self.__sim_count = sim_count
+    @number_of_sim.setter
+    def number_of_sim(self, number_of_sim: int):
+        if number_of_sim > 0:
+            self.__number_of_sim = number_of_sim
         else:
             raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
 
     def __repr__(self):
-        return f'Phone({self.name}, {self.price}, {self.quantity}, {self.sim_count}'
+        return f"{self.__class__.__name__}('{self.name}', {int(self.price)}, {self.quantity}, {self.number_of_sim})"
+
+
+phone1 = Phone("iPhone 14", 120_000, 5, 2)
+print(phone1.__repr__())
+print(phone1.__str__())
