25ff6aa3f0a02a12b0802b282b9d9dabf37cc6f0
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-19 21:46:17 +0300

    add test_phone_init

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -6,3 +6,11 @@ from src.phone import Phone
 def phone():
     return Phone("iPhone 14", 120_000, 5, 2)
 
+
+def test_phone_init(phone):
+    """
+    Тест конструктора класса Phone
+    """
+    assert str(phone) == 'iPhone 14'
+    assert repr(phone) == "Phone('iPhone 14', 120000, 5, 2)"
+    assert phone.number_of_sim == 2
