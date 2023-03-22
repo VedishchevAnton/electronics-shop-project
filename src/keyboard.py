eca9571f649dfbec2b67126cf462ad48b0e84142
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-22 21:21:47 +0300

    change class MixinLanguage

--- src/keyboard.py
+++ src/keyboard.py
@@ -3,9 +3,23 @@ from src.item import Item
 
 class Keyboard(Item):
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name: str, price: float, quantity: int, language="EN") -> None:
         """
         Создание экземпляра класса Keyboard
         """
         super().__init__(name, price, quantity)
-        self.language = "EN"
+        self.language = language
+
+
+class MixinLanguage:
+
+    def __init__(self):
+        self.language = None
+
+    def change_lang(self):
+        if self.language == "EN":
+            self.language = "RU"
+        elif self.language == "RU":
+            self.language = "EN"
+        else:
+            raise ValueError("Invalid language")
