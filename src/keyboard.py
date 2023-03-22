2da8b06d9beca03032176201ebaeab540a52d30e
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-22 21:42:24 +0300

    change setter language in Keyboard

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,14 +1,15 @@
 from src.item import Item
+from src.mixin import MixinLanguage
 
 
-class Keyboard(Item):
+class Keyboard(Item, MixinLanguage):
 
     def __init__(self, name: str, price: float, quantity: int, language="EN") -> None:
         """
         Создание экземпляра класса Keyboard
         """
         super().__init__(name, price, quantity)
-        self.__language = language
+        self.language = language
 
     @property
     def language(self):
@@ -16,4 +17,7 @@ class Keyboard(Item):
 
     @language.setter
     def language(self, lang):
-        pass
+        if lang == "EN" or lang == "RU":
+            self.__language = lang
+        else:
+            raise AttributeError("Unsupported language")
