c641a46341495edebdcdca5927b71e102d6b6ab0
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-22 21:27:30 +0300

    change class Keyboard and add getter and setter

--- src/keyboard.py
+++ src/keyboard.py
@@ -8,18 +8,12 @@ class Keyboard(Item):
         Создание экземпляра класса Keyboard
         """
         super().__init__(name, price, quantity)
-        self.language = language
+        self.__language = language
 
+    @property
+    def language(self):
+        return self.__language
 
-class MixinLanguage:
-
-    def __init__(self):
-        self.language = None
-
-    def change_lang(self):
-        if self.language == "EN":
-            self.language = "RU"
-        elif self.language == "RU":
-            self.language = "EN"
-        else:
-            raise ValueError("Invalid language")
+    @language.setter
+    def language(self, lang):
+        pass
