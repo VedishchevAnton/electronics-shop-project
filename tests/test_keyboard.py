efaa66c8086b92d19f5df12654bd32c4bc22af58
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-23 22:22:39 +0300

    add test init class KeyBoard

---
+++ tests/test_keyboard.py
@@ -0,0 +1,17 @@
+import pytest
+from src.keyboard import KeyBoard
+
+
+@pytest.fixture
+def test_keyboard():
+    return KeyBoard('DEXP Sparkle', 4500, 2)
+
+
+def test__init__(test_keyboard):
+    """
+    Тест инициализации класса KeyBoard
+    """
+    assert str(test_keyboard.name) == "DEXP Sparkle"
+    assert str(test_keyboard.language) == "EN"
+
+
