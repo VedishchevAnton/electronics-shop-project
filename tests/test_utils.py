0b7bc5132df4b6dce9aa6f753e138d69d53776de
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-27 20:41:26 +0300

    добавлены тесты take_from_csv

---
+++ tests/test_utils.py
@@ -0,0 +1,18 @@
+import pytest
+from src.utils import take_from_csv
+
+
+@pytest.fixture
+def data_csv():
+    return take_from_csv()
+
+
+def test_take_from_csv_exists(data_csv):
+    assert data_csv is not None
+
+
+def test_take_from_csv_correct_data(data_csv):
+    data_list = data_csv
+    assert len(data_list) > 0
+    assert isinstance(data_list[0], list)
+    assert isinstance(data_list[0][0], str)
