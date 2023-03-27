33fe7660a629e9f95c3a36e0b5e459254fb79aca
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-27 20:49:44 +0300

    добавление теста на работу исключения в take_from_csv

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -11,6 +11,11 @@ def test_take_from_csv_exists(data_csv):
     assert data_csv is not None
 
 
+def test_take_from_csv_exists_2(data_csv):
+    with pytest.raises(FileNotFoundError):
+        raise FileNotFoundError('Отсутствует файл item.csv')
+
+
 def test_take_from_csv_correct_data(data_csv):
     data_list = data_csv
     assert len(data_list) > 0
