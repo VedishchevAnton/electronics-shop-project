b3905cdff682bf79661593b90a4d125f8db86422
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-27 19:09:38 +0300

    добавлена обработка исключений в take_from_csv

--- src/utils.py
+++ src/utils.py
@@ -6,11 +6,15 @@ def take_from_csv() -> list:
     Получение данных из файла csv
     """
     data_list = []
-    with open('../src/items.csv', 'r') as csvfile:
-        data_csv = csv.reader(csvfile, delimiter=',')
-        for elem in data_csv:
-            if elem[0] == 'name':
-                continue
-            else:
-                data_list.append(elem)
-        return data_list
+    try:
+        with open('../src/items.csv', 'r') as csvfile:
+            data_csv = csv.reader(csvfile, delimiter=',')
+            for elem in data_csv:
+                if elem[0] == 'name':
+                    continue
+                else:
+                    data_list.append(elem)
+            return data_list
+    except FileNotFoundError:
+        print("Отсутствует файл item.csv")
+    print('Продолжение работы программы...')
