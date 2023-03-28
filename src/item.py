e84e5c96fe9cea179149269bd80c673fa589262b
Anton Vedischev <vedishchevanton@gmail.com>
2023-03-28 22:28:09 +0300

    удаление класса исключения из файа item.py

--- src/item.py
+++ src/item.py
@@ -1,16 +1,6 @@
 import csv
 
 
-class InstantiateCSVError(Exception):
-    """Класс-исключение для проверки файлов .csv"""
-
-    def __init__(self, *args) -> None:
-        self.message = args[0] if args else 'Файл item.csv поврежден'
-
-    def __str__(self) -> str:
-        return self.message
-
-
 class Item:
     """
     Класс для представления товара в магазине.
